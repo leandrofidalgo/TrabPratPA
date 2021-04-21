607b666bfed3f2ab7bdfa655ec13283d34b42b8f
leandro adão <lfidalgo.10@gmail.com>
2021-04-22 00:10:56 +0100

    começar o jogo finalmente

--- TrabPratPA/src/main/java/pt/isec/laf/jogo/iu/texto/IUTexto.java
+++ TrabPratPA/src/main/java/pt/isec/laf/jogo/iu/texto/IUTexto.java
@@ -2,6 +2,8 @@ package pt.isec.laf.jogo.iu.texto;
 
 import java.util.Scanner;
 import pt.isec.laf.jogo.logica.MaquinaDeEstados;
+import static pt.isec.laf.jogo.logica.dados.DadosJogo.COLUNAS;
+import static pt.isec.laf.jogo.logica.dados.DadosJogo.LINHAS;
 
 /**
  *
@@ -24,18 +26,29 @@ public class IUTexto {
         //inicioDoJogo();
         //menuPrincipal();
         while (maquinaDeEstados.getDadosJogo().isJogoAcabou()) {
+            System.out.println();
+            showMsgLog();
+            maquinaDeEstados.getDadosJogo().clearMsgLog();
             if (maquinaDeEstados.isMenuPrincipal()) {
                 menuPrincipal();
             } else if (maquinaDeEstados.isEscolherModoJogo()) {
                 menuEscolherModoJogo();
-            } else if(maquinaDeEstados.isModoCPUXCPU()){
+            } else if (maquinaDeEstados.isModoCPUXCPU()) {
                 menuCPUXCPU();
-            }else if(maquinaDeEstados.isModoPessoaXCPU()){
+            } else if (maquinaDeEstados.isModoPessoaXCPU()) {
                 menuPessoaXCPU();
-            }else if(maquinaDeEstados.isModoPessoaXPessoa()){
+            } else if (maquinaDeEstados.isModoPessoaXPessoa()) {
                 menuPessoaXPessoa();
-            } else if(maquinaDeEstados.isEscolherProximoJogador()){
-                menuEscolheProximoJogador();
+            } else if (maquinaDeEstados.isEscolherProximoJogador()) {
+                menuEscolherProximoJogador();
+            } else if (maquinaDeEstados.isProximaJogada4Linha()) {
+                menuProximaJogada();
+            } else if (maquinaDeEstados.isEscolherJogarMiniJogo()) {
+                menuJogarMiniJogo();
+            } else if (maquinaDeEstados.isMiniJogoCalculos()) {
+                miniJogoCalculos();
+            } else if (maquinaDeEstados.isMiniJogoPalavras()) {
+                miniJogoPalavras();
             }
         }
     }
@@ -61,6 +74,8 @@ public class IUTexto {
 
         } else if (valor == 3) {
             maquinaDeEstados.getDadosJogo().setJogoAcabou(true);
+        } else {
+            System.out.println("Valor inválido!");
         }
     }
 
@@ -94,7 +109,7 @@ public class IUTexto {
             scanner.next();
         }
         nome2 = scanner.next();
-        if(nome != null && nome2 != null){
+        if (nome != null && nome2 != null) {
             maquinaDeEstados.definirNomes(nome, nome2);
         }
     }
@@ -112,7 +127,7 @@ public class IUTexto {
             scanner.next();
         }
         nome2 = scanner.next();
-        if(nome != null && nome2 != null){
+        if (nome != null && nome2 != null) {
             maquinaDeEstados.definirNomes(nome, nome2);
         }
     }
@@ -130,13 +145,171 @@ public class IUTexto {
             scanner.next();
         }
         nome2 = scanner.next();
-        if(nome != null && nome2 != null){
+        if (nome != null && nome2 != null) {
             maquinaDeEstados.definirNomes(nome, nome2);
         }
     }
 
-    private void menuEscolheProximoJogador() {
+    private void menuEscolherProximoJogador() {
         maquinaDeEstados.escolheProximoJogador();
-        //imprimir quem vai ser o primeiro jogador
     }
+
+    private void menuProximaJogada() {
+        int valor;
+        imprimirTabuleiro();
+        System.out.println("------------------------ Menu Jogada ------------------------");
+        System.out.println("1-Efetuar uma jogada");
+        System.out.println("2-Efetuar a jogada da peça especial");
+        System.out.println("2-Retroceder jogada");
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+        }
+        valor = scanner.nextInt();
+        if (valor == 1) {
+            //chamar função para avançar uma jogada
+            int coluna;
+            System.out.print("Indique a coluna onde deseja colocar a nova peca: ");
+            while (!scanner.hasNextInt()) {
+                scanner.next();
+            }
+            coluna = scanner.nextInt();
+            maquinaDeEstados.jogaPeca(coluna);
+        } else if (valor == 2) {
+            //chamar funcao para jogar a peca especial
+            //TODO perguntar qual é a coluna onde a deseja colocar
+            maquinaDeEstados.jogaPecaEspecial();
+        } else if (valor == 3) {
+            //chamar função para perguntar quantas vezes deseja voltar atrás
+            //chamar processo de retroceder jogada
+        }
+    }
+
+    public void imprimirTabuleiro() {
+        StringBuilder sB = new StringBuilder();
+        for (int i = 0; i < LINHAS; i++) {
+            sB.append("|");
+            for (int j = 0; j < COLUNAS; j++) {
+                if (maquinaDeEstados.getDadosJogo().getTabuleiro()[i][j] == 0) {
+                    sB.append(" |");
+                }
+                if (maquinaDeEstados.getDadosJogo().getTabuleiro()[i][j] == 1) {
+                    sB.append("O|");
+                }
+                if (maquinaDeEstados.getDadosJogo().getTabuleiro()[i][j] == 2) {
+                    sB.append("X|");
+                }
+            }
+            sB.append("\n");
+        }
+        System.out.println(sB.toString());
+    }
+
+    private void menuJogarMiniJogo() {
+        int valor;
+        System.out.println("Parabéns, tem a opção de jogar um mini jogo para ter a oportunidade de lhe calhar uma peça especial!");
+        System.out.println("Deseja jogar o mini jogo?");
+        System.out.println("1-Sim");
+        System.out.println("2-Nao");
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+        }
+        valor = scanner.nextInt();
+        if (valor >= 1 && valor <= 2) {
+            maquinaDeEstados.jogarMiniJogo();
+        }
+    }
+
+    private void miniJogoCalculos() {
+        //pedir os varios numeros ao utilizador
+        int numAcertou = 0;
+        /*Thread td1 = new Thread(() -> {
+            //Thread secundaria para tratar da obtencao dos calculos do jogador
+            while (true) {
+                int numAcertou = 0;
+        int valor; //valor do calculo
+        long timepassed; //time in seconds
+        int randomSinal;
+        int randomValor1;
+        int randomValor2;
+        //gerar random para o sinal da operação 1-4
+        randomSinal = rand.nextInt(4) + 1;
+        //gerar rando para os valores a calcular 0-99
+        randomValor1 = rand.nextInt(100);
+        randomValor2 = rand.nextInt(100);
+        switch (randomSinal) {
+            case 1:
+                System.out.println(randomValor1 + " + " + randomValor2);
+                while (!scanner.hasNextInt()) {
+                    scanner.next();
+                }
+                valor = scanner.nextInt();
+                //numAcertou = maquinaDeEstados.miniJogoCalculos(valor, randomValor1, randomValor2, "+");
+                if (valor == (randomValor1 + randomValor2)) {
+                    numAcertou++;
+                }
+                break;
+            case 2:
+                System.out.println(randomValor1 + " - " + randomValor2);
+                while (!scanner.hasNextInt()) {
+                    scanner.next();
+                }
+                valor = scanner.nextInt();
+                if (valor == (randomValor1 - randomValor2)) {
+                    numAcertou++;
+                }
+                break;
+            case 3:
+                System.out.println(randomValor1 + " / " + randomValor2);
+                while (!scanner.hasNextInt()) {
+                    scanner.next();
+                }
+                valor = scanner.nextInt();
+                if (valor == (randomValor1 / randomValor2)) {
+                    numAcertou++;
+                }
+                break;
+            case 4:
+                System.out.println(randomValor1 + " x " + randomValor2);
+                while (!scanner.hasNextInt()) {
+                    scanner.next();
+                }
+                valor = scanner.nextInt();
+                if (valor == (randomValor1 * randomValor2)) {
+                    numAcertou++;
+                }
+                break;
+            default:
+                System.out.println("Existe um problema com o sinal da operação!\n");
+                return -1;
+        }
+            }
+        });
+        td1.start();
+
+        long starttime = System.currentTimeMillis();
+        try {
+            //Esperar 30 segundos na thread principal
+            contador(starttime);
+            //supostamente passou 30sec 
+            synchronized (td1) {
+                td1.notify();
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }*/
+    }
+
+    private void miniJogoPalavras() {
+        //pedir as palavras.... e ainda fazer toda a logica
+    }
+
+    public void showMsgLog() {
+        if (maquinaDeEstados.getDadosJogo().getMsgLog().size() > 0) {
+            for (String msg : maquinaDeEstados.getDadosJogo().getMsgLog()) {
+                System.out.println("---> " + msg);
+            }
+        }
+    }
+
 }
